0e12926db92b6431500c40b2e07a034fa1cfefa6
Jakub Gros <kubagros@gmail.com>
2020-11-08 11:59:21 +0100

    feat: add mechanism that will store the book on processing failure

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from database import Database
 from user_interface import UserInterface
 
 if __name__ == "__main__":
-    book = Book("book.epub")
+    db = Database()
 
+    book = Book("book.epub")
     ui = UserInterface()
 
-    ui.process(book)
+    try:
+        ui.process(book)
+    except:
+        db.store_book(book)
+    else:
+        db.store_book(book)
 
-    db = Database()
-    db.store_book(book)
-
-
-
-#TODO jagros crash recover
 #TODO jagros make it possible to close program in the middle of processing and then restart it and continue
 #TODO jagros add command to switch to next_unprocessed_word
 # TODO jagros make file storage function that behaves as a set
