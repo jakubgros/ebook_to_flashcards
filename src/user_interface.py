f2de5eab67309df2d559194b94927f7b0672af4f
Jakub Gros <kubagros@gmail.com>
2020-11-09 19:01:02 +0100

    feat: added help command which displays all available commands

--- src/user_interface.py
+++ src/user_interface.py
@@ -4,22 +4,24 @@ from src.iterator import Iterator
 
 
 class UserInterface:
-    ANSWER_KNOWN = "KNOWN"
+    ANSWER_KNOWN = "KNOWN" #TODO jagros use enums instead
     ANSWER_UNKNOWN = "UNKNOWN"
     PREVIOUS = "PREVIOUS"
     NEXT = "NEXT"
     QUIT = "QUIT"
     NEXT_NOT_PROCESSED = "NEXT_NOT_PROCESSED"
     PREVIOUS_NOT_PROCESSED = "PREVIOUS_NOT_PROCESSED"
+    HELP = "HELP"
 
     input_to_event_mapping = {
-        "q": ANSWER_KNOWN,
-        "w": ANSWER_UNKNOWN,
-        "back": PREVIOUS,
-        "fwd": NEXT,
-        "quit": QUIT,
-        "fwd np": NEXT_NOT_PROCESSED,
-        "back np": PREVIOUS_NOT_PROCESSED,
+        "q": (ANSWER_KNOWN, "Answer known"),
+        "w": (ANSWER_UNKNOWN, "Answer unknown"),
+        "back": (PREVIOUS, "Go back"),
+        "fwd": (NEXT, "Go forward"),
+        "quit": (QUIT, "Quit and save"),
+        "fwd np": (NEXT_NOT_PROCESSED, "Go forward to next unprocessed word"),
+        "back np": (PREVIOUS_NOT_PROCESSED, "Go back to previous unprocessed word"),
+        "help": (HELP, "Displays all commands"),
     }
 
     def get_event(self, event_prompt):
@@ -27,7 +29,7 @@ class UserInterface:
             answ = input(event_prompt)
 
             if answ in self.input_to_event_mapping:
-                return self.input_to_event_mapping[answ]
+                return self.input_to_event_mapping[answ][0]
 
             print(">> Invalid input, try again")
             continue
@@ -77,10 +79,15 @@ class UserInterface:
                 success = it.next(lambda w: not w.is_checked)
                 if not success:
                     self._display_info("Everything from current point till the end has been processed. Can't jump forward to next unprocessed word. ")
+
             elif event == UserInterface.PREVIOUS_NOT_PROCESSED:
                 success = it.previous(lambda w: not w.is_checked)
                 if not success:
                     self._display_info("Everything from current point till the beggining has been processed. Can't jump backward to prevuous unprocessed word. ")
+
+            elif event == UserInterface.HELP:
+                for command, (_, description) in self.input_to_event_mapping.items():
+                    print(f"\t{command} = {description}")
 
             elif event == UserInterface.QUIT:
                 return
