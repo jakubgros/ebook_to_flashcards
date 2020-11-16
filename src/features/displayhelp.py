da3043e4d8fa842ae6f7bf80d2cd91aaf9aa2004
Jakub Gros <kubagros@gmail.com>
2020-11-16 21:16:11 +0100

    refactor: changed kwargs to named arguments in features

--- src/features/displayhelp.py
+++ src/features/displayhelp.py
@@ -6,6 +6,6 @@ class DisplayHelp(Feature):
     def __init__(self):
         pass
 
-    def run(self, interface, **kwargs):
-        for command, (_, description) in kwargs['input_to_event_mapping'].items():
+    def run(self, interface, *, input_to_event_mapping, **kwargs):
+        for command, (_, description) in input_to_event_mapping.items():
             interface.display_info(f"\t{command} = {description}")
--- src/features/interrogate_to_mark_known_words_feature.py
+++ src/features/interrogate_to_mark_known_words_feature.py
@@ -4,7 +4,6 @@ from src.book import Book
 from src.database import Database
 from src.event_translator import EventTranslator
 from src.event_handler import EventHandler
-from src.features import displayhelp
 from src.features.displayhelp import DisplayHelp
 from src.features.feature import Feature
 from src.iterator import Iterator
@@ -14,36 +13,36 @@ from enum import Enum, auto
 
 
 class ANSWER_KNOWN_feat(Feature):
-    def run(self, interface, **kwargs):
-        kwargs['word'].mark_if_known(True)
-        kwargs['it'].next()
+    def run(self, interface, *, word, it, **kwargs):
+        word.mark_if_known(True)
+        it.next()
 
 class ANSWER_UNKNOWN_feat(Feature):
-    def run(self, interface, **kwargs):
-        kwargs['word'].mark_if_known(False)
-        kwargs['it'].next()
+    def run(self, interface, *, word, it, **kwargs):
+        word.mark_if_known(False)
+        it.next()
 
 class PREVIOUS_feat(Feature):
-    def run(self, interface, **kwargs):
-        success = kwargs['it'].previous()
+    def run(self, interface, *, it, **kwargs):
+        success = it.previous()
         if not success:
             interface.display_info("can't go back")
 
 class NEXT_feat(Feature):
-    def run(self, interface, **kwargs):
-        success = kwargs['it'].next()
+    def run(self, interface, *, it, **kwargs):
+        success = it.next()
         if not success:
             interface.display_info("can't go forward")
 
 class NEXT_NOT_PROCESSED_feat(Feature):
-    def run(self, interface, **kwargs):
-        success = kwargs['it'].next(lambda w: not w.is_checked)
+    def run(self, interface, *, it, **kwargs):
+        success = it.next(lambda w: not w.is_checked)
         if not success:
             interface.display_info("Everything from current point till the end has been processed. Can't jump forward to next unprocessed word. ")
 
 class PREVIOUS_NOT_PROCESSED_feat(Feature):
-    def run(self, interface, **kwargs):
-        success = kwargs['it'].previous(lambda w: not w.is_checked)
+    def run(self, interface, *, it, **kwargs):
+        success = it.previous(lambda w: not w.is_checked)
         if not success:
             interface.display_info("Everything from current point till the beggining has been processed. Can't jump backward to prevuous unprocessed word. ")
 
@@ -102,7 +101,8 @@ class InterrogateToMarkKnownWordsFeature(Feature):
             event_str = interface.get_input(event_prompt, input_validator=event_translator.is_valid)
             event = event_translator.translate(event_str)
             if event == self.EventTypes.QUIT:
-                return self.EventTypes.QUIT
+                pass
+                #TODO stack.unwind()
 
             ret = self.event_handler.process(interface, event, it=it, idx=idx, word=word, size=len(it), input_to_event_mapping=self.input_to_event_mapping)
 
